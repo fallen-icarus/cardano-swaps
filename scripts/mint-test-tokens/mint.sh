c413a3cefd821120950101bfa5d8d978149445b8
fallen-icarus <modern.daidalos+git@gmail.com>
2023-06-07 10:26:03 -0400

    added more token examples to mint script

--- scripts/mint-test-tokens/mint.sh
+++ scripts/mint-test-tokens/mint.sh
@@ -1,5 +1,8 @@
 alwaysSucceedSymbol="c0f8644a01a6bf5db02f4afe30d604975e63dd274f1098a1738e561d"
-tokenName=$(echo -n "OtherToken" | xxd -ps)
+tokenName1=$(echo -n "TestToken1" | xxd -ps)
+tokenName2=$(echo -n "TestToken2" | xxd -ps)
+tokenName3=$(echo -n "TestToken3" | xxd -ps)
+tokenName4=$(echo -n "TestToken4" | xxd -ps)
 tmpDir="../../assets/tmp/"
 
 cardano-cli query protocol-parameters \
@@ -7,20 +10,23 @@ cardano-cli query protocol-parameters \
   --out-file "${tmpDir}protocol.json"
 
 cardano-cli transaction build \
-  --tx-in 2d3bad47abe57524ad627bb11a9ed0ff22f06538b6ba6794834155615f6c212a#0 \
-  --tx-out "$(cat ../../assets/wallets/02.addr) 2000000 lovelace + 1000 ${alwaysSucceedSymbol}.${tokenName}" \
-  --mint "1000 ${alwaysSucceedSymbol}.${tokenName}" \
+  --tx-in f0ff75deb465f9bfb00b164329462297afba04243ed56a72e77430ef86c0e24a#1 \
+  --tx-out "$(cat ../../assets/wallets/01.addr) 2000000 lovelace + 1000 ${alwaysSucceedSymbol}.${tokenName1}" \
+  --tx-out "$(cat ../../assets/wallets/01.addr) 2000000 lovelace + 1000 ${alwaysSucceedSymbol}.${tokenName2}" \
+  --tx-out "$(cat ../../assets/wallets/01.addr) 2000000 lovelace + 1000 ${alwaysSucceedSymbol}.${tokenName3}" \
+  --tx-out "$(cat ../../assets/wallets/01.addr) 2000000 lovelace + 1000 ${alwaysSucceedSymbol}.${tokenName4}" \
+  --mint "1000 ${alwaysSucceedSymbol}.${tokenName1} + 1000 ${alwaysSucceedSymbol}.${tokenName2} + 1000 ${alwaysSucceedSymbol}.${tokenName3} + 1000 ${alwaysSucceedSymbol}.${tokenName4}" \
   --mint-script-file alwaysSucceedsMintingPolicy.plutus \
   --mint-redeemer-file unit.json \
-  --tx-in-collateral af3b8901a464f53cb69e6e240a506947154b1fedbe89ab7ff9263ed2263f5cf5#0 \
-  --change-address $(cat ../../assets/wallets/02.addr) \
+  --tx-in-collateral 80b6d884296198d7eaa37f97a13e2d8ac4b38990d8419c99d6820bed435bbe82#0 \
+  --change-address $(cat ../../assets/wallets/01.addr) \
   --protocol-params-file "${tmpDir}protocol.json" \
   --testnet-magic 1 \
   --out-file "${tmpDir}tx.body"
 
 cardano-cli transaction sign \
   --tx-body-file "${tmpDir}tx.body" \
-  --signing-key-file ../../assets/wallets/02.skey \
+  --signing-key-file ../../assets/wallets/01.skey \
   --testnet-magic 1 \
   --out-file "${tmpDir}tx.signed"
 
